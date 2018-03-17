bb4a6a4712e20399ec5f6d55634577de7903cc4c
David Michael Simanjuntak <david.juntak11@gmail.com>
2018-03-17 22:57:10 +0700

    Initial commit Tutorial05

---
+++ tutorial5/src/main/java/com/example/tutorial5/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.tutorial5.service;
+
+import java.util.List;
+
+import com.example.tutorial5.model.StudentModel;
+
+public interface StudentService
+{
+    StudentModel selectStudent (String npm);
+
+    List<StudentModel> selectAllStudents ();
+
+    void addStudent (StudentModel student);
+
+    void updateStudent (StudentModel student);
+
+    void deleteStudent (String npm);
+}
